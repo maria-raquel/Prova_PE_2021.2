ed48e186cb88c69315de82ce21f7a2ccddc80e1f
JonasFOliveira <jff.oliveira2018@gmail.com>
2022-05-20 20:56:53 -0300

    criacao das funcoes para a plotagem de histogramas

---
+++ histogram.h
@@ -0,0 +1,12 @@
+/*
+>>>>>>>>>>> PROVA_PROGRAMAÇÃO_ESTRUTURADA <<<<<<<<<<<<<
+
+* Professor: Igor Malheiros
+
+>>>>>>>>>>>>>>>>>>>>>>>>>|<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+Alunos:
+- Jonas Felipe | 20210026948
+- Maria Raquel | 20200025900
+*/
+
+void plot_hist(int *notas);
