0ce617836019d0bd1a267e4566b44c0572d0b6c4
JonasFOliveira <jff.oliveira2018@gmail.com>
2022-05-20 20:58:22 -0300

    criacao do main.c

---
+++ main.c
@@ -0,0 +1,17 @@
+/*
+>>>>>>>>>>> PROVA_PROGRAMAÇÃO_ESTRUTURADA <<<<<<<<<<<<<
+
+* Professor: Igor Malheiros
+
+>>>>>>>>>>>>>>>>>>>>>>>>>|<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+Alunos:
+- Jonas Felipe | 20210026948
+- Maria Raquel | 20200025900
+*/
+
+#include <stdio.h>
+
+int main(){
+    int notas[13] = {5, 3, 3, 8, 7, 10, 10, -5, 2, 7, 12, 9,10};
+    
+}
