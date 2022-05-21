35fc6bfe44fa7d41ca43f4be16dcc7e5015dbcef
JonasFOliveira <jff.oliveira2018@gmail.com>
2022-05-21 00:01:54 -0300

    histogram.c Update

--- histogram.c
+++ histogram.c
@@ -9,24 +9,34 @@ Alunos:
 - Maria Raquel | 20200025900
 */
 #include <stdio.h>
-void plot_hist(int *notas, int tam, int symb_code);
+void plot_hist(int *notas, int tam);
 
-void plot_hist(int *notas, int tam, int symb_code){
+void plot_hist(int *notas, int tam){
     int freq[11] = {0};
     for(int i = 0; i < tam; i++){
         freq[notas[i]]++;
     }
     printf("---- Histograma ------\n");
-    for (int i = 0; i < 11; i++){
+    for (int i = 0; i < 6; i++){
         printf("%.2d|",i);
         for (int j = 0; j < freq[i]; j++){
-            printf("%c", symb_code);
+            printf("◼");
         }
-        printf("\n");
+        printf("\t(:c)\n");
+    }
+    for (int i = 6; i < 11; i++){
+    printf("%.2d|",i);
+    for (int j = 0; j < freq[i]; j++){
+        printf("◼");
+    }
+    printf("\t(:D)\n");
     }
 }
 
+
 int main(){
     int notas[13] = {5, 3, 3, 8, 7, 10, 10, 0, 2, 7, 12, 9, 10};
-    plot_hist(notas, 13, 254);
+    plot_hist(notas, 13);
+
+
 }
