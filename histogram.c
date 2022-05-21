39b9fb223b90b5d9c9779d6c4609220ee2207d8a
JonasFOliveira <jff.oliveira2018@gmail.com>
2022-05-20 21:40:15 -0300

    criacao do histograma inicial

--- histogram.c
+++ histogram.c
@@ -8,9 +8,25 @@ Alunos:
 - Jonas Felipe | 20210026948
 - Maria Raquel | 20200025900
 */
+#include <stdio.h>
+void plot_hist(int *notas, int tam, int symb_code);
 
-void plot_hist(int *notas);
+void plot_hist(int *notas, int tam, int symb_code){
+    int freq[11] = {0};
+    for(int i = 0; i < tam; i++){
+        freq[notas[i]]++;
+    }
+    printf("---- Histograma ------\n");
+    for (int i = 0; i < 11; i++){
+        printf("%.2d|",i);
+        for (int j = 0; j < freq[i]; j++){
+            printf("%c", symb_code);
+        }
+        printf("\n");
+    }
+}
 
-void plot_hist(int *notas){
-    
+int main(){
+    int notas[13] = {5, 3, 3, 8, 7, 10, 10, 0, 2, 7, 12, 9, 10};
+    plot_hist(notas, 13, 254);
 }
