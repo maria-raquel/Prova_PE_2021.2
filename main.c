286f363e6f1555b4558d492750ee01f80a00f2c9
maria-raquel <mariaraquelmartinez97@gmail.com>
2022-05-23 18:06:35 -0300

    versao final do main.c

--- main.c
+++ main.c
@@ -10,73 +10,30 @@ Alunos:
 */
 
 #include <stdio.h>
-// #include <stdlib.h>
-
-// void printar_arrays(int *array, int n){
-//     for(int i=0; i<n; i++)
-//         printf("%d ", array[i]);
-//     putchar('\n');
-// }
-
-// void bubble_sort(int *notas, int n){
-//     int temp = 0;
-//     int troca;
-//     do{
-//         troca = 0;
-//         for(int i = 0; i < n-1; i++){
-//             if (notas[i] > notas[i+1]){
-//                 temp = notas[i];
-//                 notas[i] = notas[i+1];
-//                 notas[i+1] = temp;
-//                 troca = 1;
-//             }
-//         }
-//     }while(troca);  
-// }
-
-// int repeticoes_nota(int *notas, int i){
-//     if (notas[i] != notas[i+1])
-//         return 0;
-//     if (notas[i] == notas[i+1])
-//         return 1 + repeticoes_nota(notas, i+1);
-// }
-
-// int *moda(int *notas, int n){
-
-//     int modas[n], *modas_limpas, counts[n], maior_repeticao = 0, repeticoes = 0;
-
-//     counts[n-1] = 0;
-    
-//     for(int i=0; i<n-1; i++)
-//         counts[i] = repeticoes_nota(notas, i);
-
-//     for(int i=0; i<n; i++)
-//         if (counts[i] > maior_repeticao)
-//             maior_repeticao = counts[i];
-
-//     for(int i=0; i<n; i++){
-//         if (counts[i] == maior_repeticao){
-//             modas[i] = notas[i];
-//             repeticoes++;
-//         }
-//         else modas[i] = -1;
-//     }
-    
-//     modas_limpas = malloc(sizeof(int)*repeticoes);
-
-//     for(int i=0, j=0; i<n; i++)
-//         if (modas[i] != -1)
-//             modas_limpas[j++] = modas[i];
-
-//     return modas_limpas;
-// }
+#include "funcoes.h"
 
 int main(){
-    int notas[13] = {5, 3, 3, 8, 7, 10, 10, -5, 2, 7, 12, 9, 10};
+    int notas[13] = {5, 3, 3, 3, 7, 10, 10, 7, 2, 7, 12, 9, 10};
+    int *modas;
     int n = 13;
-    return 0;
 
-    printf("oi");
+    clear(notas, n);
+    ord(notas, n);
+
+    printf("Media aritimedica: %f\n", media(notas, n));
+    printf("Mediana: %f\n", mediana(notas, n));
+    
+    int tamanho_modas = 0, *tm;
+    tm = &tamanho_modas;
+    modas = moda(notas, n, tm);
+    printf("Moda(s): ");
+    printar_arrays(modas, tamanho_modas);
+
+    printf("Desvio padrao: %f\n", desvio_padrao(notas, n));
+    printf("Valor maximo: %d\n", valor_maximo(notas, n));
+    printf("Valor minimo: %d\n", valor_minimo(notas, n));
+
+    plot_hist(notas, n);
 
     return 0;
 }
